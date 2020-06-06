a1cba309bb1fd023c60cd77d707cbc8563f51f2f
gurnameh-99 <gurunameh.work@gmail.com>
2020-06-06 20:09:53 +0530

    added registration functionality

--- application.py
+++ application.py
@@ -1,11 +1,12 @@
 import os
 
-from flask import Flask, session, render_template
+from flask import Flask, session, render_template, redirect, url_for, request, flash
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
 app = Flask(__name__)
+app.secret_key = 'i dont know'
 
 # Check for environment variable
 if not os.getenv("DATABASE_URL"):
@@ -21,6 +22,38 @@ engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
 
-@app.route("/")
+@app.route("/", methods=["POST", "GET"])
 def index():
-    return render_template("index.html")
+    if request.method == "GET":
+        return render_template("index.html")
+    else:
+        username = request.form.get("LoginUsr")
+        password = request.form.get("LoginPass")
+
+        if db.execute("SELECT * FROM users WHERE username = :username AND password = :password", {"username": username, "password": password}).rowcount == 1:
+            return redirect(url_for('home'))
+        else:
+            flash('invalid username or password', 'error')
+            return redirect(url_for('index'))
+
+@app.route("/register_1", methods=["POST"])
+def register_1():
+    name = request.form.get("nm")
+    username = request.form.get("usrnm")
+    email = request.form.get("em")
+    password = request.form.get("pw")
+
+    if(name == "" or username == "" or email == "" or password == ""):
+        flash('Please fill all the required fields.', 'error')
+        return redirect(url_for('register'))
+
+    if db.execute("SELECT * FROM users WHERE username = :username", {"username": username}).rowcount == 0:
+        db.execute("INSERT INTO users (name, username, email, password) VALUES (:name, :username, :email, :password)", {"name": name, "username": username, "email": email, "password": password})
+        db.commit()
+        return render_template("success.html")
+    flash('user already registered.', 'error')
+    return redirect(url_for('register'))
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
